d6ac69b31a4e0e42134d286397efdb2b9cc22e96
yusun23 <160289921+yusun23@users.noreply.github.com>
2025-01-23 20:06:56 -0700

    Update user_app1.h

--- firmware_common/application/user_app1.h
+++ firmware_common/application/user_app1.h
@@ -59,8 +59,8 @@ static void UserApp1SM_Error(void);
 Constants / Definitions
 **********************************************************************************************************************/
 #define U8_ANT_CHANNEL_USERAPP (u8)ANT_CHANNEL_0    /* Channel 0 – 7 */
-#define U8_ANT_DEVICE_LO_USERAPP (u8)0x45           /* Low byte of two-byte Device # */ 
-#define U8_ANT_DEVICE_HI_USERAPP (u8)0x45           /* High byte of two-byte Device # */ 
+#define U8_ANT_DEVICE_LO_USERAPP (u8)0xBE           /* Low byte of two-byte Device # */ 
+#define U8_ANT_DEVICE_HI_USERAPP (u8)0x20           /* High byte of two-byte Device # */ 
 #define U8_ANT_DEVICE_TYPE_USERAPP (u8)1            /* 1 – 255 */ 
 #define U8_ANT_TRANSMISSION_TYPE_USERAPP (u8)1      /* 1-127 (MSB is pairing bit) */  
 #define U8_ANT_CHANNEL_PERIOD_LO_USERAPP (u8)0x00   /* Low byte of two-byte channel period */ 
