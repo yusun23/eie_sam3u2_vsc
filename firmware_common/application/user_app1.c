c50405d37055116c054e398fa4aa3eb7d913e02c
yusun23 <160289921+yusun23@users.noreply.github.com>
2025-02-21 01:06:52 +0100

    Progress

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -202,8 +202,6 @@ const u8 WeirdBlockRight[U8_LCD_IMAGE_ROW_PIXEL_BLOCK3][U8_LCD_IMAGE_COLUMN_PIXE
   {0x21, 0x00},
   {0x3F, 0x00}
   };
-  static u8 moveDown = 0;
-  static u8 moveSideways = 0;
   static u16 memoryImage[128][64] = {};
         
 /**********************************************************************************************************************
@@ -211,7 +209,7 @@ Function Definitions
 **********************************************************************************************************************/
 int random(int count);
 void assignment(int blockType, PixelBlockType *address, u8 **destination);
-void move(u8 downwards, u8 sideways);
+void move(u8 downwards, int sideways, PixelBlockType *block);
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @publicsection */                                                                                            
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -297,45 +295,61 @@ static void UserApp1SM_Idle(void)
 {
   // Variables for Ongoing function
   static int count = 0;
-  static u8 time = 0;
-  PixelBlockType location;
+  static u16 time = 1;
+  static PixelBlockType location;
   u8 spawn = 0;
-  static u8 downIncrease = 0;
-  static u8 firstPress = 0;
-  u8 *address = NULL;
+  u8 downIncrease = 0;
+  static int moveSideways = 0;
+  static u8 generate = 0;
+  static u8 *address = NULL;
   // Count for randomization
   if (count == 2147483647)
     count = 0;
   count++;
   spawn = random(count);
   // First press only
-  if (firstPress == 1)
+  if (generate == 1)
+  {
     time++;
+  }
   // Generate first shape
-  if ((WasButtonPressed(BUTTON0) || WasButtonPressed(BUTTON1)) && firstPress == 0)
+  if ((WasButtonPressed(BUTTON0) || WasButtonPressed(BUTTON1)) && generate == 0)
   {
     assignment(spawn, &location, &address);
     ButtonAcknowledge(BUTTON0);
-    firstPress = 1;
+    ButtonAcknowledge(BUTTON1);
+    generate = 1;
+    LcdClearScreen();
     LcdLoadBitmap(address, &location);
   }
   // Move Left and Right
-  if (WasButtonPressed(BUTTON1))
-  {
-    ButtonAcknowledge(BUTTON1);
-    if (moveSideways < 63)
-      moveSideways += 2;
-  }
-  else if (WasButtonPressed(BUTTON0))
+  if (WasButtonPressed(BUTTON0) && generate == 1 && location.u16RowSize < 63)
   {
     ButtonAcknowledge(BUTTON0);
-    if (moveSideways > 0)
-    moveSideways -= 2;
+    moveSideways = 1;
+  }
+  else if (WasButtonPressed(BUTTON1) && generate == 1 && location.u16RowSize > 0)
+  {
+    ButtonAcknowledge(BUTTON1);
+    moveSideways = -1;
   }
   // Continuous Downwards increase
-  if (time % 150 == 0)
-    downIncrease += 2;
-  move(downIncrease, moveSideways);
+  if (time % 1000 == 0)
+  {
+    if(time < 108000)
+    {
+      downIncrease = 1;
+    }
+    else
+    {
+      time = 0;
+      generate = 0;
+    }
+    move(downIncrease, moveSideways, &location);
+    moveSideways = 0;
+    LcdClearScreen();
+    LcdLoadBitmap(address, &location);
+  }
 } /* end UserApp1SM_Idle() */
      
 
@@ -394,15 +408,19 @@ void assignment(int blockType, PixelBlockType *address, u8 **destination)
         *destination = &TBlock[0][0];
         break;
     }
-    address->u16RowStart = 25 + moveDown;
-    address->u16ColumnStart = 0 + moveSideways;
+    address->u16RowStart = 25;
+    address->u16ColumnStart = 0;
     address->u16RowSize = rowSize;
     address->u16ColumnSize = columnSize;
 }
 
-void move(u8 downwards, u8 sideways)
+void move(u8 downwards, int sideways, PixelBlockType *block)
 {
-  
+  block->u16ColumnStart += downwards;
+  if (sideways == -1)
+    block->u16RowStart--;
+  else if (sideways == 1)
+    block->u16RowStart++;
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* End of File                                                                                                        */
